8aa075bb14c7bd62bfa73f0262379474d504b4ef
Asaf Davidov <adavidov@conncoll.edu>
2018-09-18 02:25:08 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,9 +3,9 @@ def count_elements(array)
   new_hash = {}
   array.each do |word|
     if new_hash.has_key?(word) 
-      new_hash[word] = 1
-    else
       new_hash[word] += 1
+    else
+      new_hash[word] = 1
     end
   end
   new_hash
